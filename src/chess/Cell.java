68b69f1df3d039cc97bdab878261c4e9f816f223
vaijaaaaa <vaijuwalker111@gmail.com>
2025-10-23 12:41:25 +0530

    Refactor Cell class: add piece management methods and improve structure

--- src/chess/Cell.java
+++ src/chess/Cell.java
@@ -3,4 +3,45 @@
 
 public class Cell {
     private int row;
+    private int col;
+    private  Piece piece;
+
+
+
+    public Cell(int row, int col){
+        this.row = row;
+        this.col = col;
+        this.piece = null;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getCol(){
+        return col;
+    }
+
+    public Piece getPiece(){
+        return piece;
+    }
+
+    public void setPiece(Piece piece){
+        this.piece = piece;
+    }
+
+    public void removePiece(){
+        this.piece = null;
+    }
+
+    @Override
+    public String toString(){
+        if(piece == null){
+            return "-";
+        }
+        return piece.toString();
+    }
+
+
+
 }
